9abcf37eacaf5624d72cb7a2736193c516dd8489
DrDesten <>
2022-05-23 13:01:34 +0200

    fixed relative directory paths

--- bin/index.js
+++ bin/index.js
@@ -1,100 +1,100 @@
 #!/usr/bin/env node
-const fs  = require("fs")
-const alg = require("./algorithms")
-const col = require("./colors")
-const helper = require("./helper")
-function print(x, color = "") { console.log(`${color}${x}${col.reset}`) }
+const fs = require( "fs" )
+const alg = require( "./algorithms" )
+const col = require( "./colors" )
+const helper = require( "./helper" )
+function print( x, color = "" ) { console.log( `${color}${x}${col.reset}` ) }
 
-function uniq(a) {
+function uniq( a ) {
   var seen = {}
-  return a.filter(function(item) {
-      return seen.hasOwnProperty(item) ? false : (seen[item] = true)
-  });
+  return a.filter( function ( item ) {
+    return seen.hasOwnProperty( item ) ? false : ( seen[ item ] = true )
+  } )
 }
 
-const isFunction  = /[A-z](?=\([A-z]\))/g
+const isFunction = /[A-z](?=\([A-z]\))/g
 const isNumerical = /^([0-9. +\-\/*()]*|[0-9.]+e[0-9.]+|Infinity)*$/g
-const letters     = /[A-z]+(?!\()/g
+const letters = /[A-z]+(?!\()/g
 
 const MATHfunctions = /(abs|acosh|acos|asinh|asin|atan2|atanh|atan|cbrt|ceil|clz32|cosh|cos|expm1|exp|floor|fround|hypot|imul|log10|log1p|log2|log|max|min|pow|random|round|sign|sinh|sin|sqrt|tanh|tan|trunc)(?=\()/g
 
-let input = process.argv.slice(2).join(" ") // Getting the argument
-print(col.dim + "> " + col.reset + col.bright + input)
+let input = process.argv.slice( 2 ).join( " " ) // Getting the argument
+print( col.dim + "> " + col.reset + col.bright + input )
 
-input = input.replace(MATHfunctions, "Math.$&")
+input = input.replace( MATHfunctions, "Math.$&" )
 
 // Simply Evaluate if there are no variables
-if (isNumerical.test(input)) {
-  let result = eval(input)
-  print(` = ${result}`, col.mathResult)
-  alg.rationalize(result)
+if ( isNumerical.test( input ) ) {
+  let result = eval( input )
+  print( ` = ${result}`, col.mathResult )
+  alg.rationalize( result )
   process.exit()
 }
 
 // Open Functions File
-let functionDatabase = fs.readFileSync("data/functions.txt", {encoding:'utf8', flag:'r'})
+let functionDatabase = fs.readFileSync( __dirname + "/../data/functions.txt", { encoding: 'utf8', flag: 'r' } )
 
-let   savedFunctions = functionDatabase.match(/(?<=const +\$)[A-z]/g)
-if  (savedFunctions == null) savedFunctions = []
-const functionNames  = "fghijklmnopqrstuvwxyzabcde".split("")
-let   savedVariables = functionDatabase.match(/(?<=const +)[A-z]/g)
-if  (savedVariables == null) savedVariables = []
-const variableNames  = "abcdefghijklmnopqrstuvwxyz".split("")
+let savedFunctions = functionDatabase.match( /(?<=const +\$)[A-z]/g )
+if ( savedFunctions == null ) savedFunctions = []
+const functionNames = "fghijklmnopqrstuvwxyzabcde".split( "" )
+let savedVariables = functionDatabase.match( /(?<=const +)[A-z]/g )
+if ( savedVariables == null ) savedVariables = []
+const variableNames = "abcdefghijklmnopqrstuvwxyz".split( "" )
 
 
-let availableFunctionNames = functionNames.filter(x => savedFunctions.findIndex(y => y == x) == -1)
-let availableVariableNames = variableNames.filter(x => savedVariables.findIndex(y => y == x) == -1)
+let availableFunctionNames = functionNames.filter( x => savedFunctions.findIndex( y => y == x ) == -1 )
+let availableVariableNames = variableNames.filter( x => savedVariables.findIndex( y => y == x ) == -1 )
 
-function evalRuntime(str) {
-  str.replace(/const *(.+?) *= *(?!>)(.+)/g, (match, g1, g2) => {
-    globalThis[g1] = eval(g2)
-  })
+function evalRuntime( str ) {
+  str.replace( /const *(.+?) *= *(?!>)(.+)/g, ( match, g1, g2 ) => {
+    globalThis[ g1 ] = eval( g2 )
+  } )
 }
 
 //input = input.replace(/[A-z]+(?=\()/g, "$$$&")
 //if (variables.length > 0) functionDatabase += `\nconst $${availableFunctionNames[0]} = (${variables.join(",")}) => ${input}`
-evalRuntime(functionDatabase)
+evalRuntime( functionDatabase )
 
 let execute = input
 
 const argRegex = /^(generate_dev|table|tbl|integral|integrate|int|solve)(\[([^^n()]+)\])?/g
-let tmp = argRegex.exec(execute)
+let tmp = argRegex.exec( execute )
 let args
-if (tmp == null) args = [""]
+if ( tmp == null ) args = [ "" ]
 else args = [
-  tmp[1] == null ? "" : tmp[1],
-  tmp[3] == null ? "" : tmp[3].split(",").map(x => parseFloat(x)),
+  tmp[ 1 ] == null ? "" : tmp[ 1 ],
+  tmp[ 3 ] == null ? "" : tmp[ 3 ].split( "," ).map( x => parseFloat( x ) ),
 ]
 //print(args)
 
-execute = execute.replace(argRegex,"").trim()
+execute = execute.replace( argRegex, "" ).trim()
 
-let variables = execute.match(letters)
-if (variables == null) variables = []
-else                   variables = uniq(variables)
+let variables = execute.match( letters )
+if ( variables == null ) variables = []
+else variables = uniq( variables )
 //print(variables)
 
-switch (args[0]) {
+switch ( args[ 0 ] ) {
   case "tbl":
   case "table":
-    alg.table(eval(`(${"x"}) => ${execute}`), ...args[1])
+    alg.table( eval( `(${"x"}) => ${execute}` ), ...args[ 1 ] )
     break
   case "int":
   case "integral":
   case "integrate":
-    alg.integrate(eval(`(${"x"}) => ${execute}`), ...args[1])
+    alg.integrate( eval( `(${"x"}) => ${execute}` ), ...args[ 1 ] )
     break
   case "solve":
-    alg.solve(eval(`(${"x"}) => ${execute.replace(/ *= *[0.e]+$/g, "").replace(/(.*?) *= *(.*)/g, "($1) - ($2)")}`), ...args[1])
+    alg.solve( eval( `(${"x"}) => ${execute.replace( / *= *[0.e]+$/g, "" ).replace( /(.*?) *= *(.*)/g, "($1) - ($2)" )}` ), ...args[ 1 ] )
     break
   case "generate_dev":
     helper.generate()
     break
-  default: 
-    let result = eval(input)
-    print(` = ${result}`, col.mathResult)
-    alg.rationalize(result)
+  default:
+    let result = eval( input )
+    print( ` = ${result}`, col.mathResult )
+    alg.rationalize( result )
 }
-  
+
 
 //fs.writeFileSync("data/functions.txt", functionDatabase)
