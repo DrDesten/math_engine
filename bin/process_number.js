365019a2b129e2b20c91d58ad0c94cf0790fa35f
DrDesten <>
2022-05-25 02:07:05 +0200

    finished rationalisation refactor

--- bin/process_number.js
+++ bin/process_number.js
@@ -5,152 +5,7 @@ function roundSig( n, p ) { return parseFloat( n.toPrecision( p ) ) }
 function roundFix( n, p ) { return parseFloat( n.toFixed( p ) ) }
 
 function processNumber( x ) {
-    if ( Math.trunc( x ) == x ) return
 
-    let fractions = []
-    let error = 1
-    for ( let i = 1; ( i <= 16777216 && error > 0 ); i++ ) {
-        let err = Math.abs( Math.round( x * i ) - ( x * i ) )
-        if ( err < error ) {
-            // [Zähler, Nenner]
-            fractions.push( [Math.round( x * i ), i] )
-            error = err
-        }
-    }
-
-    let otherFractions = []
-    const constants = [
-        [3.141592653589793, 'π'],
-        [2.718281828459045, 'e'],
-        [1.618033988749895, 'Φ'],
-        [1.7724538509055159, '√π'],
-        [1.6487212707001282, '√e'],
-        [1.272019649514069, '√Φ'],
-        [9.869604401089358, 'π²'],
-        [7.3890560989306495, 'e²'],
-        [2.618033988749895, 'Φ²'],
-        [0.3183098861837907, 'π⁻¹'],
-        [0.36787944117144233, 'e⁻¹'],
-        [0.6180339887498948, 'Φ⁻¹'],
-    ]
-    const roots = [
-        [1.4142135623730951, '√2'],
-        [1.7320508075688772, '√3'],
-        [2.23606797749979, '√5'],
-        [2.449489742783178, '√6'],
-        [2.6457513110645907, '√7'],
-        [3.1622776601683795, '√10'],
-        [3.3166247903554, '√11'],
-        [3.605551275463989, '√13'],
-        [3.7416573867739413, '√14'],
-        [3.872983346207417, '√15'],
-        [4.123105625617661, '√17'],
-        [4.358898943540674, '√19'],
-        [4.58257569495584, '√21'],
-        [4.69041575982343, '√22'],
-        [4.795831523312719, '√23'],
-        [5.0990195135927845, '√26'],
-        [5.385164807134504, '√29'],
-        [5.477225575051661, '√30'],
-        [5.5677643628300215, '√31'],
-        [5.744562646538029, '√33'],
-        [5.830951894845301, '√34'],
-        [5.916079783099616, '√35'],
-        [6.082762530298219, '√37'],
-        [6.164414002968976, '√38'],
-        [6.244997998398398, '√39'],
-        [6.4031242374328485, '√41'],
-        [6.48074069840786, '√42'],
-        [6.557438524302, '√43'],
-        [6.782329983125268, '√46'],
-        [6.855654600401044, '√47'],
-        [7.14142842854285, '√51'],
-        [7.280109889280518, '√53'],
-        [7.416198487095663, '√55'],
-        [7.54983443527075, '√57'],
-        [7.615773105863909, '√58'],
-        [7.681145747868608, '√59'],
-        [7.810249675906654, '√61'],
-        [7.874007874011811, '√62'],
-        [8.06225774829855, '√65'],
-        [8.12403840463596, '√66'],
-        [8.18535277187245, '√67'],
-        [8.306623862918075, '√69'],
-        [8.366600265340756, '√70'],
-        [8.426149773176359, '√71'],
-        [8.54400374531753, '√73'],
-        [8.602325267042627, '√74'],
-        [8.774964387392123, '√77'],
-        [8.831760866327848, '√78'],
-        [8.888194417315589, '√79'],
-        [9.055385138137417, '√82'],
-        [9.1104335791443, '√83'],
-        [9.219544457292887, '√85'],
-        [9.273618495495704, '√86'],
-        [9.327379053088816, '√87'],
-        [9.433981132056603, '√89'],
-        [9.539392014169456, '√91'],
-        [9.643650760992955, '√93'],
-        [9.695359714832659, '√94'],
-        [9.746794344808963, '√95'],
-        [9.848857801796104, '√97']
-    ]
-    const checkConstants = constants.map( c => x / c[0] )
-    error = 1e-3
-    for ( let m = 1; ( m <= 3600 && error > 0 ); m++ ) {
-        let errConstants = checkConstants.map( check => Math.abs( Math.round( check * m ) - roundSig( check * m, 15 ) ) )
-        for ( let i = 0; i < errConstants.length; i++ ) {
-            if ( errConstants[i] < error ) {
-                let fraction = [Math.round( checkConstants[i] * m ), m]
-                otherFractions.push( [
-                    fraction[0],
-                    fraction[1],
-                    constants[i][1], errConstants[i] * ( Math.abs( fraction[0] ) + fraction[1] )
-                ] )
-                error = errConstants[i]
-            }
-        }
-    }
-
-    const checkRoots = roots.map( c => x / c[0] )
-    let rootError = 1e-5
-    for ( let m = 1; ( m <= 64 && rootError > 0 ); m++ ) {
-        let errRoots = checkRoots.map( check => Math.abs( Math.round( check * m ) - roundSig( check * m, 15 ) ) )
-        for ( let i = 0; i < errRoots.length; i++ ) {
-            if ( errRoots[i] < rootError ) {
-                let fraction = [Math.round( checkRoots[i] * m ), m]
-                otherFractions.push( [
-                    fraction[0],
-                    fraction[1],
-                    roots[i][1],
-                    errRoots[i] * ( Math.abs( fraction[0] ) + fraction[1] )
-                ] )
-                rootError = errRoots[i]
-            }
-        }
-    }
-
-    if ( otherFractions.length > 0 ) {
-        otherFractions.sort( ( a, b ) => a[3] - b[3] )
-        let frac = otherFractions[0]
-        if ( frac[0] != 0 ) {
-            let sign = frac[0] / frac[1] < 0
-            frac[0] = Math.abs( frac[0] )
-            print( ` ${frac[3] == 0 ? "=" : "≈"} ${sign ? "-" : ""}${frac[0] == 1 ? frac[2] : frac[0]}${frac[1] == 1 ? "" : "/" + frac[1]}${frac[0] == 1 ? "" : ( frac[1] == 1 ? "" : " * " ) + frac[2]} `, frac[3] == 0 ? col.mathResult : col.mathOtherResult )
-        }
-    }
-
-    let errors = fractions.map( fraction => Math.abs( fraction[0] / fraction[1] - x ) ).filter( ( x, i, a ) => ( i == 0 || a[i - 1] != 0 ) )
-    fractions = fractions.filter( ( x, i ) => ( i < errors.length ) )
-    let weightedErrors = errors.map( ( err, i ) => err * ( Math.abs( fractions[i][0] ) * Math.abs( fractions[i][1] ) + 1 ) ) // Errors weighted by their rational complexity
-
-    let sortedFractions = fractions.map( ( fraction, i ) => [fraction[0], fraction[1], weightedErrors[i]] ).sort( ( a, b ) => a[2] - b[2] )
-
-    for ( let i = 0; i < Math.min( 2, sortedFractions.length ); i++ ) {
-        let fraction = sortedFractions[i]
-        if ( fraction[2] == 0 ) print( ` = ${fraction[0]}${fraction[1] == 1 ? "" : "/" + fraction[1]} `, col.mathOtherResult )
-        else print( ` ≈ ${fraction[0]}${fraction[1] == 1 ? "" : "/" + fraction[1]} `, col.mathOtherResult )
-    }
 }
 
 
@@ -195,6 +50,56 @@ function rationalizeConstants( x, maxDenominator = 3600 ) {
 
 }
 
+function rationalizeRoots( x, maxDenominator = 32 ) {
+
+    const roots = [[1.4142135623730951, "√2"], [1.7320508075688772, "√3"], [2.23606797749979, "√5"], [2.449489742783178, "√6"], [2.6457513110645907, "√7"], [3.1622776601683795, "√10"], [3.3166247903554, "√11"], [3.605551275463989, "√13"], [3.7416573867739413, "√14"], [3.872983346207417, "√15"], [4.123105625617661, "√17"], [4.358898943540674, "√19"], [4.58257569495584, "√21"], [4.69041575982343, "√22"], [4.795831523312719, "√23"], [5.0990195135927845, "√26"], [5.385164807134504, "√29"], [5.477225575051661, "√30"], [5.5677643628300215, "√31"], [5.744562646538029, "√33"], [5.830951894845301, "√34"], [5.916079783099616, "√35"], [6.082762530298219, "√37"], [6.164414002968976, "√38"], [6.244997998398398, "√39"], [6.4031242374328485, "√41"], [6.48074069840786, "√42"], [6.557438524302, "√43"], [6.782329983125268, "√46"], [6.855654600401044, "√47"], [7.14142842854285, "√51"]]
+    const checkRoots = roots.map( c => x / c[0] )
+    const checkRootInverses = roots.map( c => x * c[0] )
+    let rootFractions = []
+
+    let error = 0.05
+    for ( let denom = 1; ( denom <= maxDenominator && error > 0 ); denom++ ) {
+
+        let errRoots = checkRoots.map( check => Math.abs( Math.round( check * denom ) - ( check * denom ) ) )
+        let errRootInverses = checkRootInverses.map( check => Math.abs( Math.round( check * denom ) - ( check * denom ) ) )
+
+        for ( let i = 0; i < errRoots.length; i++ ) {
+            if ( errRoots[i] < error || errRootInverses[i] < error ) {
+
+                let thisError = Math.min( errRoots[i], errRootInverses[i] )
+                let fraction = errRootInverses[i] < errRoots[i] ?
+                    [Math.round( checkRootInverses[i] * denom ), denom] :
+                    [Math.round( checkRoots[i] * denom ), denom]
+                rootFractions.push( [
+                    fraction[0],
+                    fraction[1],
+                    roots[i][1],
+                    thisError * ( fraction[0] ** 2 + fraction[1] ** 2 ),
+                    errRootInverses[i] < errRoots[i] // is inverse or not
+                ] )
+                error = thisError
+
+            }
+        }
+
+    }
+
+    if ( rootFractions.length == 0 ) return
+    rootFractions.sort( ( a, b ) => a[3] - b[3] )
+
+    let frac = rootFractions[0]
+    if ( frac[0] != 0 && frac[3] < 0.1 ) {
+        let sign = frac[0] / frac[1] < 0
+        frac[0] = Math.abs( frac[0] )
+        if ( frac[1] == 1 ) // Denominator = 1
+            print( ` ${frac[3] == 0 ? "=" : "≈"} ${sign ? "-" : ""}${frac[0] != 1 || frac[4] ? frac[0] : ""}${frac[4] ? "/" : ""}${frac[2]}`, frac[3] == 0 ? col.mathResult : col.mathOtherResult )
+        else
+            print( ` ${frac[3] == 0 ? "=" : "≈"} ${sign ? "-" : ""}${frac[0]}/${frac[1]}${frac[4] ? "" : " * "}${frac[2]}`, frac[3] == 0 ? col.mathResult : col.mathOtherResult )
+    }
+
+}
+
+
 
 // Get the physical constants by executing the "generate_dev" command
 function matchConstants( x ) {
@@ -222,6 +127,7 @@ function matchConstants( x ) {
 
 function rationalize( x, maxDenominator = 16777216 ) {
     rationalizeConstants( x )
+    rationalizeRoots( x )
     matchConstants( x )
     if ( Math.trunc( x ) == x ) return
 
